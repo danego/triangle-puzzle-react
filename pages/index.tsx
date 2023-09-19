48e83de1b7036c0bbdee8e73bbe190b1f0734540
Dane Neves <nevesdane@gmail.com>
2023-09-19 10:23:09 -0600

    gcp configs and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Head>
         <title>Triangle Puzzle Solver</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
